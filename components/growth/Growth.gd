06fc1486aacc9d6bd7cadec494075603a58ad139
Olivier PEREZ <olivier@olivierperez.fr>
2024-11-04 20:06:09 +0100

    feat(item-resource): move growth mechanism in Growth component

---
+++ components/growth/Growth.gd
@@ -0,0 +1,48 @@
+class_name Growth
+extends Node
+
+## Seconds between growth tick
+@export var tick_time: int = 3600
+@export var initial_quantity: int = 0
+@export var max_quantity: int = 1
+
+@onready var timer: Timer = %Timer
+
+
+signal _on_quantity_change(int)
+
+
+var quantity := 0:
+	set(value):
+		quantity = value
+		_on_quantity_change.emit(value)
+
+
+#region built-in
+
+func _ready() -> void:
+	assert(get_parent() is ItemResource, "Growth must be child of ItemResource")
+	quantity = initial_quantity
+	if tick_time > 0:
+		timer.start(tick_time)
+
+#endregion built-in
+
+
+#region logic
+
+func on_collect() -> void:
+	quantity = 0
+	timer.start(tick_time)
+
+#endregion login
+
+
+#region signal
+
+func _on_timeout() -> void:
+	quantity += 1
+	if quantity >= max_quantity:
+		timer.stop()
+
+#endregion signal
--- objects/item/stone/resource/StoneResource.gd
+++ objects/item/stone/resource/StoneResource.gd
@@ -1,53 +1,27 @@
 class_name StoneResource
 extends ItemResource
 
-
-@onready var growth_timer: Timer = %GrowthTimer
-
-
-const MAX_QUANTITY := 2
-
-var _quantity := 0:
-	set(value):
-		_quantity = value
-		_on_quantity_change(value)
-
-
-#region built-in
-
-func _ready() -> void:
-	super._ready()
-	_quantity = MAX_QUANTITY
-	growth_timer.start()
-
-#endregion built-in
+@onready var growth: Growth = %Growth
 
 
 #region logic
 
 func collect() -> ItemData:
-	var quantity_to_drop = _random_quantity_to_drop(_quantity)
-	_quantity = 0
-	growth_timer.start()
+	var quantity_to_drop = _random_quantity_to_drop(growth.quantity)
+	growth.on_collect()
 	return ItemData.new(ItemRefs.REF_STONE, quantity_to_drop)
 
 
 func is_collectable() -> bool:
-	return _quantity > 0
+	return growth.quantity > 0
 
 #endregion logic
 
 
 #region private
 
-func _on_growth_timer_tick() -> void:
-	_quantity += 1
-	if _quantity >= MAX_QUANTITY:
-		growth_timer.stop()
-
-
 func _on_quantity_change(quantity: int) -> void:
-	animated_sprite.play(str(quantity))
+	%AnimatedSprite.play(str(quantity))
 
 
 func _random_quantity_to_drop(quantity: int) -> int:
