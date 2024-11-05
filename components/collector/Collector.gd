ecb960ac0478cdc67800808784e2d20be059ad67
Olivier PEREZ <olivier@olivierperez.fr>
2024-11-05 09:54:06 +0100

    feat(item-resource): rework resource selection

--- components/collector/Collector.gd
+++ components/collector/Collector.gd
@@ -24,6 +24,18 @@ func _process(delta: float) -> void:
 
 #region logic
 
+func _target_nearest_resource() -> void:
+	var resources = _area.get_overlapping_bodies().filter(_is_collectable_resource)
+	resources.sort_custom(_sort_by_distance)
+
+	var new_target: ItemResource
+	if resources.size() > 0:
+		new_target = resources[0]
+		_set_targeted_resource(new_target)
+	else:
+		_reset_targeted_resource()
+
+
 func _is_collectable_resource(body: Node2D) -> bool:
 	if body is ItemResource:
 		return body.is_collectable()
@@ -39,18 +51,18 @@ func _sort_by_distance(a: Node2D, b: Node2D) -> bool:
 
 #region private
 
-func _target_nearest_resource() -> void:
-	var resources = _area.get_overlapping_bodies().filter(_is_collectable_resource)
-	resources.sort_custom(_sort_by_distance)
+func _set_targeted_resource(target: ItemResource) -> void:
+	if target != _targeted_resource:
+		if _targeted_resource != null:
+			_targeted_resource.hide_as_collectable()
+		target.show_as_collectable()
+		_targeted_resource = target
 
+
+func _reset_targeted_resource() -> void:
 	if _targeted_resource != null:
 		_targeted_resource.hide_as_collectable()
-
-	if resources.size() > 0:
-		_targeted_resource = resources[0]
-		_targeted_resource.show_as_collectable()
-	else:
-		_targeted_resource = null
+	_targeted_resource = null
 
 
 func _detect_resource_collection() -> void:
