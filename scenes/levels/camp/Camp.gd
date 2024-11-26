0b79cbc92c7ec8d0c0c3f493da9939ad2ef7eb84
Olivier PEREZ <olivier@olivierperez.fr>
2024-11-26 21:03:49 +0100

    feat(house): clean

--- scenes/levels/camp/Camp.gd
+++ scenes/levels/camp/Camp.gd
@@ -7,6 +7,7 @@ extends Node2D
 func _ready() -> void:
 	ProgressionService.init()
 	var progression = ProgressionService.data
+	InventoryStorage.restore(progression.items)
 	for idx in houses.get_child_count():
 		var level = progression.houses_levels[idx] if idx < progression.houses_levels.size() else 0
 		(houses.get_child(idx) as House).level = level
@@ -18,8 +19,7 @@ func _on_return_to_planete_pressed() -> void:
 
 func _save() -> void:
 	var progression = ProgressionService.data
-	var houses_levels = _get_houses_levels()
-	progression.houses_levels = houses_levels
+	progression.houses_levels = _get_houses_levels()
 	progression.items = InventoryStorage.get_items()
 	ProgressionService.save(progression)
 
