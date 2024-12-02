323ff5e9b3fa3627179440189e17e654fe481c47
Olivier PEREZ <olivier@olivierperez.fr>
2024-12-02 19:32:07 +0100

    feat(objective): setup HUD for objectives

---
+++ scenes/hud/objectives/Objectives.gd
@@ -0,0 +1,12 @@
+extends PanelContainer
+
+@onready var description: Label = %Description
+@onready var finish_button: Button = %FinishButton
+
+
+func _ready() -> void:
+	pass
+
+
+func _on_finish_button_pressed() -> void:
+	pass
